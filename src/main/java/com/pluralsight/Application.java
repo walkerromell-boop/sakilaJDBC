6b30bb90cc9ec29a165524af88a5d8b03328e994
Romell Walker <walkerromell@gmail.com>
2025-12-01 13:57:32 -0500

    add throws

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -2,12 +2,13 @@ package com.pluralsight;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
         String username = "root";
-        String password = "y";
+        String password = "yearup24";
 
         // open connection to database
         Connection connection = DriverManager.getConnection(url,username,password);
