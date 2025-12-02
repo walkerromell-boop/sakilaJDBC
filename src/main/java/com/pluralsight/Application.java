cdc03bca98bb47bc23e4e90a9a0a92274dce3d07
Romell Walker <walkerromell@gmail.com>
2025-12-02 09:46:42 -0500

    add args

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,16 +1,33 @@
 package com.pluralsight;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "yearup24";
+        String username = args[0];
+        String password = args[1];
 
         // open connection to database
-        Connection connection = DriverManager.getConnection(url,username,password);
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+
+    // create statement
+    // the statement is tied to the open connection
+        Statement statement = connection.createStatement();
+
+    //define your query
+        String query = "SELECT city FROM city ";
+    // 2. Execute your query
+        ResultSet results = statement.executeQuery(query);
+    // process the results
+        while (results.next()) {
+            String city = results.getString("city");
+            System.out.println(city);
+        }
+    // 3. Close the connection
+        connection.close();
+        System.out.println(results);
+
     }
 }
